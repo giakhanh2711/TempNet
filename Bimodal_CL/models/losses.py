fe1387566726bbd242c39c218d3735e3e98009e3
giakhanh2711 <53686667+giakhanh2711@users.noreply.github.com>
2025-12-03 00:36:32 -0600

    update

--- Bimodal_CL/models/losses.py
+++ Bimodal_CL/models/losses.py
@@ -391,6 +391,7 @@ class TempGenerator(torch.nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
 
     def _init_prototypes(self, feats):
+        self.prototypes = torch.zeros(feats.size(0), feats.size(1), device=feats.device)
         self.prototypes.data.copy_(feats)
 
     def forward(self, x, return_feats=False):
